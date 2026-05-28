7db8ebacaae8d630f2bef0537f6f6f75a6e267d6
Realeboga Mohale <mohaleolerato82@gmail.com>
2026-05-28 16:29:21 +0200

    app.xaml class

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,8 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ChatApp_Part2
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
-
 }
